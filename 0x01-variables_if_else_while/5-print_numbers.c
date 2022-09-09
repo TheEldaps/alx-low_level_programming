f96ebadc10687e4f7efd67bbf61c8a514197459b
TheEldaps <adejames97@gmail.com>
2022-09-09 14:26:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int g = 0;
+	int q = 10;
+
+	for (g = 0; g < q; g++)
+		printf("%d", g);
+	printf("\n");
+
+	return (0);
+}
