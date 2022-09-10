e272a7f61863251443870a7e3e67cd2fa3216052
TheEldaps <adejames97@gmail.com>
2022-09-09 17:21:19 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int g = 0;
+	long int q = 10;
+	int n = '\n'
+
+	; while (g < q)
+	{
+		putchar(g + '0');
+		g++;
+	}
+	putchar(n);
+
+	return (0);
+}
