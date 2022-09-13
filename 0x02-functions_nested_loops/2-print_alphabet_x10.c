22f5da49c7f39aa4be5cd34a4cdd128d8edaf162
TheEldaps <adejames97@gmail.com>
2022-09-13 11:54:08 -0700

    10x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry point for the programme
+ *return - not needed since the return type is void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int zee = 'j';
+
+	for (i = 'a'; i <= zee; i++)
+	{	
+		int q = 'a';
+		int p = 'z';
+	
+		for (q = 'a'; q <= p; q++)
+		{
+			_putchar(q);
+		}
+		_putchar('\n');
+	}
+}
