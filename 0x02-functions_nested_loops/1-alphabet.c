ea62da56461faf6da04409090d12aa494ee66a61
TheEldaps <adejames97@gmail.com>
2022-09-13 10:28:00 -0700

    loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- *print_alphabet is the entry and where the execution starts 
+ *print_alphabet - is the entry and where the execution starts
+ *
  */
 
 void print_alphabet(void)
 {
-	int i = a;
-	int zee = z;
+	int i = 'a';
+	int zee = 'z';
 
-	for (i = a; i >= zee; i++)
-		_putchar (i);
+	for (i = 'a'; i <= zee; i++)
+		_putchar(i);
+	_putchar('\n');
 }
