9736a15ed0b129a5714db67e82bf83f0d98d792c
TheEldaps <adejames97@gmail.com>
2022-09-13 15:17:25 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of kts parameter
+ * @n: the larameter whose sign will be checked
+ * Return: returns 1 and orinta + if n is positive,0 and 0 if not /
+ * and -1 and - if otherwise
+ */
+
+int print_sign(int n)
+}
+	if (n > 0)
+		_putchar(43);
+		return (1);
+	else if (n < 0)
+		_putchar(45);
+		return (-1);
+	else
+		_puchar(48);
+		return (0);
+{
