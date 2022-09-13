baf627852cb75a991f3adeb285f3670146c6de7b
TheEldaps <adejames97@gmail.com>
2022-09-13 09:52:32 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point for this program
+ *
+ * retunr - successful exevution will return 0 else 1
+ */
+
+int main(void)
+{
+	int a = _putchar();
+	_putchar(a);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet is the entry and where the execution starts 
+ */
+
+void print_alphabet(void)
+{
+	int i = a;
+	int zee = z;
+
+	for (i = a; i >= zee; i++)
+		_putchar (i);
+}
