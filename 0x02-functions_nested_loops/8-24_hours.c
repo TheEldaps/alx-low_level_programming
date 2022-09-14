e4bc340f7f66dd72d947b74e0b309dc3a189665f
TheEldaps <adejames97@gmail.com>
2022-09-14 02:30:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,17 @@ void jack_bauer(void)
 
 	while (a < 24)
 	{
-
-	b = 0;
+		b = 0;
 	while (b < 60)
-		{
-					_putchar((a / 10) + '0');
-					_putchar((a % 10) + '0');
-					_putchar(':');
-					_putchar((b / 10) + '0');
-					_putchar((b % 10) + '0');
-					_putchar('\n');
-					b++;
-		}
+	{
+		_putchar((a / 10) + '0');
+		_putchar((a % 10) + '0');
+		_putchar(':');
+		_putchar((b / 10) + '0');
+		_putchar((b % 10) + '0');
+		_putchar('\n');
+		b++;
+	}
 	a++;
 	}
 }
