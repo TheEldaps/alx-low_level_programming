74017447c1ac8e5f5274bc37a3afe328c1ab162e
TheEldaps <adejames97@gmail.com>
2022-09-14 01:16:53 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - displays the times table fron 0 to 9
+ * Return: the exit location for the program
+ */
+
+
+void times_table(void)
+{
+	int a = 0;
+	int b = 0;
+
+	for (a = 0; a <= 9; a++)
+		for (b = 0; b <= 9; b++)
+			int c = a * b;
+
+			_putchar(c + '0');
+			_putchar(',');
+			_putchar(' ');
+	_putchar('\n');
+}
