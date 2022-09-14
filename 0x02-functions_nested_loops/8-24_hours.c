4fcdca5a3dac27b22477377477b91fc42af72d64
TheEldaps <adejames97@gmail.com>
2022-09-14 02:22:47 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,29 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * jackbauer - prints everytick of the secobd throughout the day
+ * jack_bauer - prints everytick of the secobd throughout the day
  * Return: the exit point for the programnd
  */
 
-void main(void)
+void jack_bauer(void)
 {
-	int a = 0, b = 0;
+	int a, b;
 
-	for (a = 0; a < 3; a++)
-		for (b = 0; b <= 9; b++)
-			if (a == 2 && b == 4)
-			{
-				break;
-			}
-			int c = 0, d = 0;
+	a = 0;
 
-			for (c = 0; c < 6; c++)
-				for (d = 0; d <= 9; d++)
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(':');
-					putchar(c + '0');
-					putchar(d + '0');
+	while (a < 24)
+	{
+
+	b = 0;
+	while (b < 60)
+		{
+					_putchar((a / 10) + '0');
+					_putchar((a % 10) + '0');
+					_putchar(':');
+					_putchar((b / 10) + '0');
+					_putchar((d % 10) + '0');
+					_putchar('\n');
+					b++;
+		}
+	a++;
+	}
 }
