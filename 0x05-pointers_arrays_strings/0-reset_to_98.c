0e68acc1e05bac38b02c28fbe40bfe908159a718
TheEldaps <adejames97@gmail.com>
2022-09-19 11:48:32 -0700

    add

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * void reset_to_98 - updates the variable at address of pointer to 98
+ * return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My tasks on pointers and arrays
