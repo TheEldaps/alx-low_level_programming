85c033df7d99f396d57a20b5c5e65e87550c2f44
TheEldaps <adejames97@gmail.com>
2022-09-30 16:56:11 -0700

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -13,12 +13,6 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s \n", argv[i]);
-	}
-	printf("\n");
-	return (0);
+	printf("%s \n", argv[0]);
+	return (argc - argc);
 }
