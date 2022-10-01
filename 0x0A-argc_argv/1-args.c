0f0644a5b9cbe55f9e3897bdd09c0265533eea9c
TheEldaps <adejames97@gmail.com>
2022-09-30 18:55:44 -0700

    2-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- *main: entry point of programme
+ *main- entry point of programme
  *Return: exit point of programme
  *@argc: parameter count
  *@argv: array of the arguments passed to the programme
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d \n", (argc - 1));
 	return (0);
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - entry point of the programme
  * Return: exit point of the programme
  *@argc: argument count
  *@argv: array of string
- *@x: index to iterate through the elements of the string array
  */
 
 int main(int argc, char *argv[])
