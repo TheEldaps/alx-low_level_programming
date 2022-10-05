720a4434784a5ac06ec07118c7f67694273c61b1
TheEldaps <adejames97@gmail.com>
2022-10-05 11:00:18 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	int los = 0;
 	int c = 0;
 
-	if (s1 == '\0' || s2 == '\0')
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	for (; *(s1 + i) != '\0'; i++)
