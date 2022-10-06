cb74755eab3b514422c4a70f5eb08309f34fdf6f
TheEldaps <adejames97@gmail.com>
2022-10-06 12:01:10 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - function that retruns a pointer to allocated memory
+ *
+ *@b: size of memory to be alloacted in bytes
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+	
+	p = malloc(b};
+	if (p = NULL)
+	{
+		return (98);
+	}
+
+	return (p);
+}
