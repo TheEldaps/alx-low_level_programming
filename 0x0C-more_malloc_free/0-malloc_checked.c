663657483bc396776281e9463e3e4c3876eef27a
TheEldaps <adejames97@gmail.com>
2022-10-06 12:07:39 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,17 +5,17 @@
  * malloc_checked - function that retruns a pointer to allocated memory
  *
  *@b: size of memory to be alloacted in bytes
- *
+ *Return: Null or 98
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
-	
+	int *p;
+
 	p = malloc(b);
-	if (p = NULL)
+	if (p == NULL)
 	{
-		return (98);
+		exit(98);
 	}
 
 	return (p);
