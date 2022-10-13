ff4f65aeb6c26fe6a0fcebeb2ba3552adbd34fe7
TheEldaps <adejames97@gmail.com>
2022-10-13 03:39:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+solutions to structures and typedef tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure that takes dog details
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
