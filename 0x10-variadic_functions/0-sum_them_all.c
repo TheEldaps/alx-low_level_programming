5d5038b8292eb2631ca0af53038306fe88421b36
TheEldaps <adejames97@gmail.com>
2022-10-17 09:59:26 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
+	int i;
 
 	if (n == 0)
 	{
@@ -18,5 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 		{
 			sum = sum + va_args;
 		}
-		return (sum);
+		return (sum)
+
+	
 }
